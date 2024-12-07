256cbacf1f2cbb54f38255c24df24b1e8e8fbe21
MoraisTP <morais05400@hotmail.com>
2024-12-07 14:35:01 +0100

    updt js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
-// Fonction pour basculer entre le mode sombre et clair
+// Fonction qui permet de basculer entre le mode sombre et mode clair
+function toggleDarkMode() {
+    document.body.classList.toggle("dark-mode");
+}
+
+// Fonction qui permet de modifier le texte de mon bouton, on verifie si le bouton est "dark", on remplace le texte à l'intérieur par son inverse.
+// Si le contraire se produit (light), on le modifier en "dark"
 function toggleDarkMode() {
     const body = document.body;
     const toggleButton = document.getElementById("dark-mode-toggle");
@@ -14,7 +20,7 @@ function toggleDarkMode() {
     }
 }
 
-// Mode dark en premier chargement
+// Au chargement de la page, execute le site en dark-mode grâce à body.classList.add("dark-mode")
 window.onload = function () {
     const body = document.body;
     const toggleButton = document.getElementById("dark-mode-toggle");
@@ -23,4 +29,18 @@ window.onload = function () {
     body.classList.add("dark-mode");
     toggleButton.textContent = "Light Mode";
 };
+
+
+// Merci à https://github.com/mattboldt/typed.js ! Effet "typed" qui permet d'avoir un effet "en train d'écrire" très simple et lèger.
+
+var typed = new Typed(".typed-text", {
+    strings: [
+        "Étudiant Développeur",
+        "Passionné par l'IA",
+        "Fan de jeux vidéo",
+    ],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+});
 
