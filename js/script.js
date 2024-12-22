f1776c4ec1174e0a6b86535ba975dc6b1b7ee4c9
MoraisTP <morais05400@hotmail.com>
2024-12-22 20:10:08 +0100

    fix light/dark + add new navbar

--- js/script.js
+++ js/script.js
@@ -1,36 +1,20 @@
-// Fonction qui permet de basculer entre le mode sombre et mode clair
-function toggleDarkMode() {
-    document.body.classList.toggle("dark-mode");
+let darkmode = localStorage.getItem('dark-mode')
+const darkModetoggle = document.getElementById('dark-mode-toggle')
+
+const enableDarkmode = () => {
+    document.body.classList.add('dark-mode')
+    localStorage.setItem('dark-mode', 'active')
 }
 
-// Fonction qui permet de modifier le texte de mon bouton, on verifie si le bouton est "dark", on remplace le texte à l'intérieur par son inverse.
-// Si le contraire se produit (light), on le modifier en "dark"
-function toggleDarkMode() {
-    const body = document.body;
-    const toggleButton = document.getElementById("dark-mode-toggle");
-
-    // Basculer la classe dark-mode
-    body.classList.toggle("dark-mode");
-
-    // Vérifie si Dark ou si Light
-    if (body.classList.contains("dark-mode")) {
-        toggleButton.textContent = "Light Mode";
-    } else {
-        toggleButton.textContent = "Dark Mode";
-    }
+const disableDarkmode = () => {
+    document.body.classList.remove('dark-mode')
+    localStorage.setItem('dark-mode', null)
 }
-
-// Au chargement de la page, execute le site en dark-mode grâce à body.classList.add("dark-mode")
-window.onload = function () {
-    const body = document.body;
-    const toggleButton = document.getElementById("dark-mode-toggle");
-
-    // Activer directement le mode sombre au chargement
-    body.classList.add("dark-mode");
-    toggleButton.textContent = "Light Mode";
-};
-
-
+if (darkmode === "active") enableDarkmode()
+darkModetoggle.addEventListener("click", () => {
+    darkmode = localStorage.getItem('dark-mode')
+    darkmode !== "active" ? enableDarkmode() : disableDarkmode()
+})
 // Merci à https://github.com/mattboldt/typed.js ! Effet "typed" qui permet d'avoir un effet "en train d'écrire" très simple et lèger.
 
 var typed = new Typed(".typed-text", {
@@ -43,4 +27,13 @@ var typed = new Typed(".typed-text", {
     backSpeed: 50,
     loop: true,
 });
+// Menu burger
+const mobileNav = document.querySelector(".hamburger");
+const navbar = document.querySelector(".menubar");
+
+const toggleNav = () => {
+    navbar.classList.toggle("active");
+    mobileNav.classList.toggle("hamburger-active");
+};
+mobileNav.addEventListener("click", () => toggleNav());
 
